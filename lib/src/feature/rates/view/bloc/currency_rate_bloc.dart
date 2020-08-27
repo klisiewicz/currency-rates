9f9731203002c002d45a978473f2bc9a123c0f97
klisiewicz <karol.lisiewicz@gmail.com>
2020-08-27 21:29:11 +0200

    Update: Dependencies.

--- lib/src/feature/rates/view/bloc/currency_rate_bloc.dart
+++ lib/src/feature/rates/view/bloc/currency_rate_bloc.dart
@@ -12,7 +12,7 @@ class CurrencyRatesBloc extends Bloc<CurrencyRateEvent, CurrencyRateState> {
 
   CurrencyRatesBloc(this._repository)
       : assert(_repository != null),
-        super(const CurrencyRatesLoaded([]));
+        super(const CurrencyRateState.loaded([]));
 
   void loadCurrencyRates() => add(const CurrencyRateEvent.load());
 
--- lib/src/feature/rates/view/bloc/currency_rate_state.freezed.dart
+++ lib/src/feature/rates/view/bloc/currency_rate_state.freezed.dart
@@ -1,5 +1,5 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named
+// ignore_for_file: deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies
 
 part of 'currency_rate_state.dart';
 
@@ -12,22 +12,26 @@ T _$identity<T>(T value) => value;
 class _$CurrencyRateStateTearOff {
   const _$CurrencyRateStateTearOff();
 
+// ignore: unused_element
   Loading loading() {
     return const Loading();
   }
 
+// ignore: unused_element
   Loaded loaded(List<CurrencyRate> rates) {
     return Loaded(
       rates,
     );
   }
 
+// ignore: unused_element
   Refreshing refreshing(List<CurrencyRate> rates) {
     return Refreshing(
       rates,
     );
   }
 
+// ignore: unused_element
   Failure failure(dynamic error) {
     return Failure(
       error,
